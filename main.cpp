90535c18d884ae48d1594631c9995cc3e7918b93
Pablo Ribalta Lorenzo <pribalta@nvidia.com>
2020-10-31 10:45:57 +0100

    Arrays and pointers and functions and pass by reference

--- main.cpp
+++ main.cpp
@@ -1,20 +1,34 @@
 #include <iostream>
-#include <array>
+#include <vector>
+
+// INPUTS ---> {???} ---> OUTPUT
+
+// void print_contents(std::vector<int>* input) {
+//     for(int i = 0; i < input->size(); i++) {
+//         std::cout << input->at(i) << std::endl;
+//     }
+// }
+
+void print_contents(std::vector<int>& input) {
+    for(int i = 0; i < input.size(); i++) {
+        std::cout << input[i] << std::endl;
+    }
+}
 
 int main() {
 
-    std::array<int, 4> numbers;
+    int size;
 
-    for (int i = 0; i < 4; i++) {
-        numbers[i] = i;
-    }
+    std::cout << "Provide a size for your array: " << std::endl;
+    std::cin >> size;
 
-    int *ptr_numbers = &numbers[0];
+    std::vector<int> numbers;
 
-    for (int i = 0; i < 4; i++) {
-        std::cout << *ptr_numbers << std::endl;
-        ++ptr_numbers;
-    }
+    for (int i = 0; i < size; i++) {
+        numbers.push_back(i);
+    }    
+
+    print_contents(numbers);
 
     return 0;
 }
