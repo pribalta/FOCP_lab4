9bd6962929d772b9eeb87f3e768fa3545017010f
Pablo Ribalta Lorenzo <pribalta@nvidia.com>
2020-10-31 11:59:28 +0100

    Example of counting occurrences of words in file

--- main.cpp
+++ main.cpp
@@ -2,25 +2,27 @@
 #include <map>
 #include <fstream>
 #include <string>
-
+#include <vector>
 
 int main() {
 
-    // word - counter
-    std::map<std::string, int> dictionary;
+    // word - list with lines where word occurs
+    std::map<std::string, std::vector<int>> dictionary;
 
     std::ifstream input("lorem.txt");
     
     if (input.is_open()) {
 
         std::string word;
+        int line_number = 0;
 
         while (getline(input, word)) {            
-            if (dictionary.find(word) == dictionary.end()) { // If reached the end of dict finding a key
-                dictionary.insert({word, 1}); // Add word to dictionary with 1 occurence
+            if (dictionary.find(word) == dictionary.end()) { // If reached the end of dict without finding a key
+                dictionary.insert({word, {line_number}}); // Add word to dictionary and line where it appears
             } else {
-                dictionary[word]++; // Increase number of occurences by 1
+                dictionary[word].push_back(line_number); // Add the line number to the index
             }
+            ++line_number;
         }
     }
 
@@ -29,7 +31,11 @@ int main() {
     std::cout << "Choose a word: " << std::endl;
     std::cin >> tmp;
 
-    std::cout << "The word " << tmp << " appears " << dictionary[tmp] << " times." << std::endl;
+    std::cout << "The word " << tmp << " appears in lines:" << std::endl;
+
+    for (int i = 0; i < dictionary[tmp].size(); i++) {
+        std::cout << dictionary[tmp][i] << std::endl;
+    }
 
     return 0;
 }
