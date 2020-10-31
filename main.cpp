0f863e4209b5c6a91d9dc2755f38339efcab7c39
Pablo Ribalta Lorenzo <pribalta@nvidia.com>
2020-10-31 09:53:31 +0100

    Modify variable from pointer

--- main.cpp
+++ main.cpp
@@ -9,5 +9,16 @@ int main() {
     std::cout << ptr << std::endl;
     // std::cout << *ptr << std::endl; -- Don't uncomment, will blow up!
 
+    std::cout << "Memory address of variable: " << &variable << std::endl;
+    std::cout << "Memory address of ptr: " << &ptr << std::endl;
+
+    ptr = &variable;
+
+    std::cout << *ptr << std::endl;
+
+    *ptr = 25;
+
+    std::cout << variable << std::endl;
+
     return 0;
 }
