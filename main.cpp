6395a3f53b1c7d55594b371c314a504cdfbb733c
Pablo Ribalta Lorenzo <pribalta@nvidia.com>
2020-10-31 09:41:38 +0100

    Null pointer exception example

--- main.cpp
+++ main.cpp
@@ -1,3 +1,13 @@
+#include <iostream>
+
 int main() {
+
+    int variable = 8;
+
+    int* ptr; // empty!!! ----> Null Pointer
+
+    std::cout << ptr << std::endl;
+    // std::cout << *ptr << std::endl; -- Don't uncomment, will blow up!
+
     return 0;
 }
